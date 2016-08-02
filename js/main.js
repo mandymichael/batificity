73070799ad5ebc424392ef4ecd16ab6fabfee474
Mandy Michael <mandymichael@outlook.com>
2016-08-02 22:10:01 +0800

    remove the horrible buggy modals and just have inline accordion style

--- js/main.js
+++ js/main.js
@@ -1,27 +1,21 @@
 $(document).ready(function() {
 	var waypoints = $('.content').waypoint({
-		handler: function(direction) {
+		handler: function() {
 			$(this.element).addClass('inview');
 		},
 		offset: '50%'
 	});
 
-	   $('#the-bomb .content').waypoint(function(event, direction) {
+	$('#the-bomb .content').waypoint(function() {
 	    $(this.element).addClass('inview');
-	  }, {offset: 900});
+	}, {offset: 900});
 
 	$('.help').click(function() {
 		var curSection = $(this).parents('section').attr('id');
-		$('#' + curSection + '-modal').fadeIn().addClass('is-visible');
-
+		$('#' + curSection + '-modal').toggleClass('is-open');
 	});
 
 	$('.button').click(function() {
-		$('.is-visible').fadeOut().removeClass('is-visible');
-		var item = $(this).attr('href');
-
-		$('html, body').animate({
-	        scrollTop: $(item).offset().top
-	    }, 1000);
-	})
+		$(this).parents('.modal').toggleClass('is-open');
+	});
 });
